6102b048ce53e7c586ca9b5810ab2dbf9e9d8262
Tri Bui <triok1505@gmail.com>
2023-06-23 23:15:17 +0700

    implement admin login

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Render view.
+     * @return string
+     */
+    public function index(): string
+    {
+        return Auth::guard('admin')->check()
+            ? redirect()->route('admin.dashboard.index') : view('admin.login');
+    }
+
+    /**
+     * Authenticate.
+     * @param LoginRequest $request
+     * @return RedirectResponse
+     */
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $request = $request->validated();
+        if (Auth::guard('admin')->attempt($request)) {
+            session()->regenerate();
+            return redirect()->route('admin.dashboard.index');
+        }
+        return back()->withErrors([
+            'status' => 'Please check your login information.',
+        ])->withInput();
+    }
+
+    /**
+     * Logout.
+     * @return RedirectResponse
+     */
+    public function logout(): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return back();
+    }
+}
